60467ac964c3582810853b5c5272d0275e7d8b89
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-20 16:48:37 +0530

    create layout for chat container

--- src/app/(home)/components/base/ChatContainer.tsx
+++ src/app/(home)/components/base/ChatContainer.tsx
@@ -5,9 +5,11 @@ import SendMessageInput from "./SendMessageInput"
 const ChatContainer = () => {
     return (
         <div className="relative rounded-xl bg-muted/50 p-4 col-span-2 max-h-[calc(100vh-90px)] overflow-hidden">
-            {/* <ChatSection />
-            <SendMessageInput /> */}
-            <Empty />
+            <>
+                <ChatSection />
+                <SendMessageInput />
+            </>
+            {/* <Empty /> */}
 
         </div>
     )
