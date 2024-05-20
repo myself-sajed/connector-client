09cf06cf13929cfacf82e68dceecad91ae848774
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-20 16:41:34 +0530

    add the empty component to the chat container

--- src/app/(home)/components/base/ChatContainer.tsx
+++ src/app/(home)/components/base/ChatContainer.tsx
@@ -1,11 +1,14 @@
 import ChatSection from "./ChatSection"
+import Empty from "./Empty"
 import SendMessageInput from "./SendMessageInput"
 
 const ChatContainer = () => {
     return (
         <div className="relative rounded-xl bg-muted/50 p-4 col-span-2 max-h-[calc(100vh-90px)] overflow-hidden">
-            <ChatSection />
-            <SendMessageInput />
+            {/* <ChatSection />
+            <SendMessageInput /> */}
+            <Empty />
+
         </div>
     )
 }
