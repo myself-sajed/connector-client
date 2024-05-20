d1ea5e08010f656bb66e90d318b910d2ac089007
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-20 10:03:09 +0530

    reduce extra padding near scrollbar

--- src/app/(home)/components/base/UserChatBar.tsx
+++ src/app/(home)/components/base/UserChatBar.tsx
@@ -5,7 +5,7 @@ const UserChatBar = () => {
     return (
         <div className="relative hidden flex-col items-start gap-2 md:flex">
             <Input type="search" placeholder="Search chats" className="sticky left-0 top-0" />
-            <div className="divide-y overflow-y-auto w-full overflow-hidden max-h-[calc(100vh-137px)] pr-10">
+            <div className="divide-y overflow-y-auto w-full overflow-hidden max-h-[calc(100vh-137px)] pr-5">
                 {
                     Array(15).fill(0).map((_, index) => (
                         <ChatUserCard key={index} />
