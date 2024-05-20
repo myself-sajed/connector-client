f4cf18ae3934d4cc03b6e6fc3fe174237ad0caa1
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-20 12:13:02 +0530

    add online status of the selected user

--- src/app/(home)/components/base/Header.tsx
+++ src/app/(home)/components/base/Header.tsx
@@ -2,12 +2,15 @@ import Image from "next/image"
 
 const Header = () => {
     return (
-        <header className="sticky top-0 z-10 h-[57px] items-center grid gap-4 p-4 md:grid-cols-2 lg:grid-cols-3  border-b bg-background px-4">
+        <header className="sticky top-0 z-10 h-[57px] items-start grid gap-4 p-4 md:grid-cols-2 lg:grid-cols-3  border-b bg-background px-4">
             <h1 className="text-xl font-semibold flex items-center"><span>Connector</span> <span className="ml-4 text-primary-foreground text-base bg-primary px-3 rounded-xl">Chats</span></h1>
 
-            <div className="flex items-center gap-5">
+            <div className="flex items-start gap-5">
                 <Image src="/assets/sender.jpg" className="rounded-full" alt="profile" height={32} width={32} />
-                <span className="font-semibold">Shaikh Sajed</span>
+                <div className="flex flex-col">
+                    <p className="font-semibold">Shaikh Sajed</p>
+                    <span className="text-xs text-muted-foreground" style={{ lineHeight: '10px' }}>online</span>
+                </div>
             </div>
         </header>
     )
