e163457c95d2dbbb7836d753bcff19b18554c7e2
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-20 14:23:32 +0530

    add bubble hover effect

--- src/app/(home)/components/base/ChatBubble.tsx
+++ src/app/(home)/components/base/ChatBubble.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 function ChatBubble({ message, isMe }: { message: string, isMe?: boolean }) {
     return (
+
         <div className={`flex ${isMe ? 'justify-end' : 'justify-start'} mb-4 text-sm`}>
             <div
-                className={`max-w-xs rounded-lg px-4 py-2  
-                ${isMe ? 'bg-blue-600 text-right text-white' : 'bg-gray-300 text-left text-black'}`}
+                className={`max-w-xs rounded-lg px-4 py-2 transition duration-200 cursor-pointer
+                ${isMe ? 'hover:bg-blue-500 bg-blue-600 text-right text-white'
+                        : 'hover:bg-gray-100 bg-gray-200 text-left text-black'}`}
                 style={{
                     borderBottomLeftRadius: isMe ? '16px' : '0',
                     borderBottomRightRadius: isMe ? '0' : '16px',
