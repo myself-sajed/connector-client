4addc049eb1201b8741c1ce81dda8863daef68ff
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-20 14:23:20 +0530

    fix header css

--- src/app/(home)/components/base/Header.tsx
+++ src/app/(home)/components/base/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const Header = () => {
     return (
-        <header className="sticky top-0 z-10 h-[57px] items-start grid gap-4 p-4 md:grid-cols-2 lg:grid-cols-3  border-b bg-background px-4">
+        <header className="sticky top-0 z-10 h-[57px] items-center grid gap-4 md:grid-cols-2 lg:grid-cols-3  border-b bg-background px-4 ">
             <h1 className="text-xl font-semibold flex items-center"><span>Connector</span> <span className="ml-4 text-primary-foreground text-base bg-primary px-3 rounded-xl">Chats</span></h1>
 
             <div className="flex items-start gap-5">
