2592b4fc4ebd23a9fe3e0b892aa0f5767814f6e6
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-21 12:57:17 +0530

    subscribe header to display current tab

--- src/app/(home)/components/base/Header.tsx
+++ src/app/(home)/components/base/Header.tsx
@@ -1,9 +1,15 @@
+import { tabs } from "@/lib/constants"
+import { RootState } from "@/redux/store"
 import Image from "next/image"
+import { useSelector } from "react-redux"
 
 const Header = () => {
+
+    const currentTab = useSelector((state: RootState) => state.active?.currentTab || tabs.CHATS)
+
     return (
         <header className="sticky top-0 z-10 h-[57px] items-center grid gap-4 md:grid-cols-2 lg:grid-cols-3  border-b bg-background px-4 ">
-            <h1 className="text-xl font-semibold flex items-center"><span>Connector</span> <span className="ml-4 text-primary-foreground text-base bg-primary px-3 rounded-xl">Chats</span></h1>
+            <h1 className="text-xl font-semibold flex items-center"><span>Connector</span> <span className="ml-4 text-primary-foreground text-base bg-primary px-3 rounded-xl">{currentTab}</span></h1>
 
             <div className="flex items-start gap-5">
                 <Image src="/assets/sender.jpg" className="rounded-full" alt="profile" height={32} width={32} />
