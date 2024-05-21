44a2d98badfbb5a864a76d807bf0aa3e33c8095b
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-21 14:36:43 +0530

    add dynamic tab rendering

--- src/app/(home)/Connector.tsx
+++ src/app/(home)/Connector.tsx
@@ -2,15 +2,30 @@ import Sidebar from "./components/base/Sidebar"
 import UserChatBar from "./components/base/UserChatBar"
 import ChatContainer from "./components/base/ChatContainer"
 import Header from "./components/base/Header"
+import { useSelector } from "react-redux"
+import { RootState } from "@/redux/store"
+import { TabType, tabs } from "@/lib/constants"
+import UserContactBar from "./components/base/UserContactBar"
 
 export default function Connector() {
+
+  const currentTab = useSelector((state: RootState) => state.active?.currentTab) as TabType
+
+  const tabMap = {
+    Chats: <UserChatBar />,
+    Contacts: <UserContactBar />,
+    Status: <div>Status</div>,
+    Settings: <div>Settings</div>,
+    Profile: <div>Profile</div>
+  }
+
   return (
     <div className="grid max-h-screen w-full pl-[56px]">
       <Sidebar />
       <div className="flex flex-col h-full">
         <Header />
         <main className="grid flex-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3 h-full">
-          <UserChatBar />
+          {tabMap?.[currentTab]}
           <ChatContainer />
         </main>
       </div>
