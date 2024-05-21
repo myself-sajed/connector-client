f5a2cc6f41312b6aed377647c5e843b436717542
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-21 18:19:52 +0530

    update chat user card to show single chat

--- src/app/(home)/components/unit/ChatUserCard.tsx
+++ src/app/(home)/components/unit/ChatUserCard.tsx
@@ -1,20 +1,38 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Badge } from '@/components/ui/badge'
+import { Chat } from '@/lib/types'
+import { setContact } from '@/redux/slices/activeSlice'
+import { useDispatch } from 'react-redux'
 
-const ChatUserCard = () => {
+type PropType = {
+    chat: Chat
+}
+
+const ChatUserCard = ({ chat }: PropType) => {
+
+    const contact = chat.contact
+    const dispatch = useDispatch()
+
+
+    const handleContactSelect = () => {
+        dispatch(setContact(contact))
+    }
+
+
+
     return (
-        <div className="flex items-start justify-between px-2 py-4 cursor-pointer hover:bg-muted transition duration-200">
+        <div className="flex items-start justify-between px-2 py-4 cursor-pointer hover:bg-muted transition duration-200" onClick={handleContactSelect}>
             <div className="flex items-start gap-4">
                 <Avatar className="hidden h-9 w-9 sm:flex">
-                    <AvatarImage src="/assets/sender.jpg" alt="Avatar" />
-                    <AvatarFallback>OM</AvatarFallback>
+                    <AvatarImage src={contact.avatar} alt="Avatar" />
+                    <AvatarFallback>{contact.name}</AvatarFallback>
                 </Avatar>
                 <div className="grid gap-1">
                     <p className="text-sm font-medium leading-none">
-                        Shaikh Sajed
+                        {contact.name}
                     </p>
                     <p className="text-sm text-muted-foreground">
-                        Hey, how are you doing???
+                        {chat.lastMessage.text}
                     </p>
                 </div>
             </div>
