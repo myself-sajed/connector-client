a3ed37e6e8d62f81f048163f63dc5ae6d0ca5c50
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-21 12:56:31 +0530

    dynamic sidebar with active current tab functionality

--- src/app/(home)/components/base/Sidebar.tsx
+++ src/app/(home)/components/base/Sidebar.tsx
@@ -1,15 +1,8 @@
 import {
-    Book,
-    Bot,
     CircleDashed,
-    Code2,
-    LifeBuoy,
     MessageCircle,
     Settings,
-    Settings2,
-    SquareTerminal,
     SquareUser,
-    Triangle,
     Users,
     Waypoints,
 } from "lucide-react"
@@ -21,9 +14,47 @@ import {
     TooltipProvider,
     TooltipTrigger,
 } from "@/components/ui/tooltip"
+import { tabs } from "@/lib/constants"
+import { useDispatch, useSelector } from "react-redux"
+import { setCurrentTab } from "@/redux/slices/activeSlice"
+import { RootState } from "@/redux/store"
+
+const upperTabs = [
+    {
+        name: tabs.CHATS,
+        icon: <MessageCircle className="size-5" />,
+    },
+    {
+        name: tabs.CONTACTS,
+        icon: <Users className="size-5" />,
+    },
+    {
+        name: tabs.STATUS,
+        icon: <CircleDashed className="size-5" />,
+    },
+]
+
+const lowerTabs = [
+    {
+        name: tabs.SETTINGS,
+        icon: <Settings className="size-5" />,
+    },
+    {
+        name: tabs.PROFILE,
+        icon: <SquareUser className="size-5" />,
+    },
+]
 
 
 const Sidebar = () => {
+
+    const dispatch = useDispatch()
+    const currentTab = useSelector((state: RootState) => state.active?.currentTab || tabs.CHATS)
+
+    const handleTabChange = (tabName: string) => {
+        dispatch(setCurrentTab(tabName))
+    }
+
     return (
         <aside className="inset-y fixed  left-0 z-20 flex h-full flex-col border-r">
             <div className="border-b p-2">
@@ -31,102 +62,55 @@ const Sidebar = () => {
                     <Waypoints strokeWidth={2.5} className="fill-foreground" />
                 </Button>
             </div>
-            <nav className="grid gap-1 p-2">
+            <nav className="grid gap-3 p-2">
 
-                <TooltipProvider>
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                className="rounded-lg bg-muted"
-                                aria-label="Chats"
-                            >
-                                <MessageCircle className="size-5" />
-                            </Button>
-                        </TooltipTrigger>
-                        <TooltipContent side="right" sideOffset={5}>
-                            Chats
-                        </TooltipContent>
-                    </Tooltip>
-                </TooltipProvider>
-
-                <TooltipProvider>
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                className="rounded-lg"
-                                aria-label="Start a new chat"
-                            >
-                                <Users className="size-5" />
-                            </Button>
-                        </TooltipTrigger>
-                        <TooltipContent side="right" sideOffset={5}>
-                            Start a new chat
-                        </TooltipContent>
-                    </Tooltip>
-                </TooltipProvider>
-
-
-
-                <TooltipProvider>
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                className="rounded-lg"
-                                aria-label="Status"
-                            >
-                                <CircleDashed className="size-5" />
-                            </Button>
-                        </TooltipTrigger>
-                        <TooltipContent side="right" sideOffset={5}>
-                            Status
-                        </TooltipContent>
-                    </Tooltip>
-                </TooltipProvider>
-
+                {
+                    upperTabs.map((tab) => {
+                        return <TooltipProvider key={tab.name}>
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <Button
+                                        variant={tab.name == currentTab ? "default" : "ghost"}
+                                        size="icon"
+                                        className={`rounded-lg`}
+                                        aria-label={tab.name}
+                                        onClick={() => handleTabChange(tab.name)}
+                                    >
+                                        {tab.icon}
+                                    </Button>
+                                </TooltipTrigger>
+                                <TooltipContent side="right" sideOffset={5}>
+                                    {tab.name}
+                                </TooltipContent>
+                            </Tooltip>
+                        </TooltipProvider>
+                    })
+                }
 
             </nav>
-            <nav className="mt-auto grid gap-1 p-2">
-                <TooltipProvider>
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                className="mt-auto rounded-lg"
-                                aria-label="Settings"
-                            >
-                                <Settings className="size-5" />
-                            </Button>
-                        </TooltipTrigger>
-                        <TooltipContent side="right" sideOffset={5}>
-                            Settings
-                        </TooltipContent>
-                    </Tooltip>
-                </TooltipProvider>
-
-                <TooltipProvider>
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                className="mt-auto rounded-lg"
-                                aria-label="Account"
-                            >
-                                <SquareUser className="size-5" />
-                            </Button>
-                        </TooltipTrigger>
-                        <TooltipContent side="right" sideOffset={5}>
-                            Account
-                        </TooltipContent>
-                    </Tooltip>
-                </TooltipProvider>
+            <nav className="mt-auto grid gap-3 p-2">
+                {
+                    lowerTabs.map((tab) => {
+                        return <TooltipProvider key={tab.name}>
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <Button
+                                        variant={tab.name == currentTab ? "default" : "ghost"}
+                                        size="icon"
+                                        className={`rounded-lg`}
+                                        aria-label={tab.name}
+                                        onClick={() => handleTabChange(tab.name)}
+                                    >
+                                        {tab.icon}
+                                    </Button>
+                                </TooltipTrigger>
+                                <TooltipContent side="right" sideOffset={5}>
+                                    {tab.name}
+                                </TooltipContent>
+                            </Tooltip>
+                        </TooltipProvider>
+                    })
+                }
             </nav>
         </aside>
     )
