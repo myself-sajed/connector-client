93b5c8f5079d6b71e19b95554a36cfdc40b673f1
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-21 08:36:37 +0530

    create a static message structure with dummy messages

--- src/app/(home)/components/base/ChatContainer.tsx
+++ src/app/(home)/components/base/ChatContainer.tsx
@@ -1,18 +1,90 @@
+"use client"
+
+import { useState } from "react"
 import ChatSection from "./ChatSection"
 import Empty from "./Empty"
 import SendMessageInput from "./SendMessageInput"
 
 const ChatContainer = () => {
+
+    const [messages, setMessages] = useState(messagesArray)
+
     return (
         <div className="relative rounded-xl bg-muted/50 p-4 col-span-2 max-h-[calc(100vh-90px)] overflow-hidden">
             <>
-                <ChatSection />
-                <SendMessageInput />
+                <ChatSection messages={messages} />
+                <SendMessageInput setMessages={setMessages} />
             </>
-            {/* <Empty /> */}
-
         </div>
     )
 }
 
 export default ChatContainer
+
+const messagesArray = [
+    {
+        isMe: true,
+        message: 'Hey!'
+    },
+    {
+        isMe: false,
+        message: 'Hii',
+    },
+    {
+        isMe: true,
+        message: "Are you coming to the party?",
+    },
+    {
+        isMe: false,
+        message: 'Yes',
+    },
+    {
+        isMe: true,
+        message: 'Meet you at 6 then!',
+    },
+    {
+        isMe: false,
+        message: 'Sure shot!!',
+    },
+    {
+        isMe: true,
+        message: 'What are you wearing?',
+    },
+    {
+        isMe: false,
+        message: 'Hoodies',
+    },
+    {
+        isMe: true,
+        message: "That's great",
+    },
+    {
+        isMe: false,
+        message: 'What are you wearing?',
+    },
+    {
+        isMe: true,
+        message: "Kurta!!!!",
+    },
+    {
+        isMe: false,
+        message: 'Kurta!!! OMG!! 😍',
+    },
+    {
+        isMe: false,
+        message: 'You are gonna look awesome😍',
+    },
+    {
+        isMe: true,
+        message: 'Thanks',
+    },
+    {
+        isMe: false,
+        message: 'Bye! See ya...',
+    },
+    {
+        isMe: true,
+        message: 'Bye. See yaaa!',
+    }
+]
+
