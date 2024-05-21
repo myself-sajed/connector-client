7101a2a2a51615342e864c788f7f872e3e9f03e0
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-21 14:45:42 +0530

    create interface for contact

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -2,3 +2,13 @@ export interface IMessages {
   isMe: boolean;
   message: string;
 }
+
+export interface Contact {
+  _id: string;
+  name: string;
+  email: string;
+  bio: string;
+  avatar: string;
+  createdAt: string;
+  updatedAt: string;
+}
