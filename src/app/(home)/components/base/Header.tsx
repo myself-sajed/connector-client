41295ca6a3835e2345c3f665da6a5b997e6d2ae6
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-21 14:43:47 +0530

    show selected user

--- src/app/(home)/components/base/Header.tsx
+++ src/app/(home)/components/base/Header.tsx
@@ -1,3 +1,4 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { tabs } from "@/lib/constants"
 import { RootState } from "@/redux/store"
 import Image from "next/image"
@@ -6,18 +7,25 @@ import { useSelector } from "react-redux"
 const Header = () => {
 
     const currentTab = useSelector((state: RootState) => state.active?.currentTab || tabs.CHATS)
+    const selectedContact = useSelector((state: RootState) => state.active?.selectedContact)
 
     return (
         <header className="sticky top-0 z-10 h-[57px] items-center grid gap-4 md:grid-cols-2 lg:grid-cols-3  border-b bg-background px-4 ">
             <h1 className="text-xl font-semibold flex items-center"><span>Connector</span> <span className="ml-4 text-primary-foreground text-base bg-primary px-3 rounded-xl">{currentTab}</span></h1>
 
-            <div className="flex items-start gap-5">
-                <Image src="/assets/sender.jpg" className="rounded-full" alt="profile" height={32} width={32} />
-                <div className="flex flex-col">
-                    <p className="font-semibold">Shaikh Sajed</p>
-                    <span className="text-xs text-muted-foreground" style={{ lineHeight: '10px' }}>online</span>
+            {
+                selectedContact && <div className="flex items-start gap-5">
+                    <Avatar className="hidden h-9 w-9 sm:flex">
+                        <AvatarImage src={selectedContact.avatar} alt="Avatar" />
+                        <AvatarFallback>{selectedContact.name}</AvatarFallback>
+                    </Avatar>
+                    <div className="flex flex-col">
+                        <p className="font-semibold">{selectedContact.name}</p>
+                        <span className="text-xs text-muted-foreground" style={{ lineHeight: '14px' }}>online</span>
+                    </div>
                 </div>
-            </div>
+            }
+
         </header>
     )
 }
