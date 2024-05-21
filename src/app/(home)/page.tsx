e190c40b0f80ec7b18bd0585af8f26ebe5baec30
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-21 14:36:59 +0530

    add staletime to queries for 10 mins

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,11 +2,26 @@
 import Connector from "./Connector"
 import { Provider as ReduxProvider } from 'react-redux'
 import { store } from "@/redux/store"
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 const Home = () => {
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 600000
+      }
+    }
+  })
+
   return (
     <ReduxProvider store={store}>
-      <Connector />
+      <QueryClientProvider client={queryClient}>
+        <Connector />
+      </QueryClientProvider>
     </ReduxProvider>
   )
 }
