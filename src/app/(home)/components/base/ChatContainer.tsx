182256ce63d7173d6c71f46db7daea61e5c61d4b
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-22 09:17:16 +0530

    show server messages

--- src/app/(home)/components/base/ChatContainer.tsx
+++ src/app/(home)/components/base/ChatContainer.tsx
@@ -6,19 +6,23 @@ import Empty from "./Empty"
 import SendMessageInput from "./SendMessageInput"
 import { RootState } from "@/redux/store"
 import { useSelector } from "react-redux"
+import { Message } from "@/lib/types"
+
 
 const ChatContainer = () => {
 
-    const [messages, setMessages] = useState(messagesArray)
+    const [messages, setMessages] = useState<Message[]>([])
     const selectedContact = useSelector((state: RootState) => state.active?.selectedContact)
+
+
 
     return (
         <div className="relative rounded-xl bg-muted/50 p-4 col-span-2 min-h-[calc(100vh-90px)] max-h-[calc(100vh-90px)] overflow-hidden">
             {
                 selectedContact
                     ? <>
-                        <ChatSection messages={messages} />
-                        <SendMessageInput setMessages={setMessages} />
+                        <ChatSection messages={messages} setMessages={setMessages} selectedContact={selectedContact} />
+                        <SendMessageInput selectedContact={selectedContact} setMessages={setMessages} />
                     </>
                     : <Empty />
             }
@@ -27,71 +31,3 @@ const ChatContainer = () => {
 }
 
 export default ChatContainer
-
-const messagesArray = [
-    {
-        isMe: true,
-        message: 'Hey!'
-    },
-    {
-        isMe: false,
-        message: 'Hii',
-    },
-    {
-        isMe: true,
-        message: "Are you coming to the party?",
-    },
-    {
-        isMe: false,
-        message: 'Yes',
-    },
-    {
-        isMe: true,
-        message: 'Meet you at 6 then!',
-    },
-    {
-        isMe: false,
-        message: 'Sure shot!!',
-    },
-    {
-        isMe: true,
-        message: 'What are you wearing?',
-    },
-    {
-        isMe: false,
-        message: 'Hoodies',
-    },
-    {
-        isMe: true,
-        message: "That's great",
-    },
-    {
-        isMe: false,
-        message: 'What are you wearing?',
-    },
-    {
-        isMe: true,
-        message: "Kurta!!!!",
-    },
-    {
-        isMe: false,
-        message: 'Kurta!!! OMG!! 😍',
-    },
-    {
-        isMe: false,
-        message: 'You are gonna look awesome😍',
-    },
-    {
-        isMe: true,
-        message: 'Thanks',
-    },
-    {
-        isMe: false,
-        message: 'Bye! See ya...',
-    },
-    {
-        isMe: true,
-        message: 'Bye. See yaaa!',
-    }
-]
-
