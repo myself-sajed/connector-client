18280e1b86ccb1aa2734982cc90b4573a41f1195
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-22 23:34:37 +0530

    add empty state if no contacts available

--- src/app/(home)/components/base/UserContactBar.tsx
+++ src/app/(home)/components/base/UserContactBar.tsx
@@ -9,10 +9,14 @@ import Loading from "@/components/ui/loading"
 import { Contact } from "@/lib/types"
 import { useSelector } from "react-redux"
 import { RootState } from "@/redux/store"
+import EmptyBar from "../unit/EmptyBar"
+import { tabs } from "@/lib/constants"
 
 const UserContactBar = () => {
 
     const user = useSelector((state: RootState) => state.user?.user) || null
+    const selectedContact = useSelector((state: RootState) => state.active?.selectedContact)
+
     const { data: contact, isLoading, isError } = useQuery({
         queryKey: ['contact-list'],
         queryFn: () => getUsers(user),
@@ -29,13 +33,19 @@ const UserContactBar = () => {
                     </Badge>
                     : isLoading
                         ? <Loading title="Fetching Contacts..." />
-                        : <div className="divide-y overflow-y-auto w-full overflow-hidden max-h-[calc(100vh-137px)] pr-5">
+                        : contact?.data?.length > 0 ? <div className="divide-y overflow-y-auto w-full overflow-hidden min-h-[calc(100vh-137px)] max-h-[calc(100vh-137px)] pr-5">
                             {
-                                contact?.data.map((contact: Contact) => (
-                                    <ChatContactCard key={contact._id} contact={contact} />
-                                ))
+                                contact?.data.map((contact: Contact) => {
+                                    return <ChatContactCard key={contact._id} contact={contact} isSelected={contact._id === selectedContact?._id} />
+                                })
                             }
                         </div>
+
+                            :
+
+                            <div className="overflow-y-auto w-full overflow-hidden min-h-[calc(100vh-137px)] max-h-[calc(100vh-137px)] pr-5">
+                                <EmptyBar type={tabs.CONTACTS} />
+                            </div>
             }
 
         </div>
