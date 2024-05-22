c523328f075c06b966f1262d9f771c4b0a050e95
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-22 09:16:18 +0530

    remove providers

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,28 +1,7 @@
-"use client"
 import Connector from "./Connector"
-import { Provider as ReduxProvider } from 'react-redux'
-import { store } from "@/redux/store"
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-
 const Home = () => {
-
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 600000
-      }
-    }
-  })
-
   return (
-    <ReduxProvider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <Connector />
-      </QueryClientProvider>
-    </ReduxProvider>
+    <Connector />
   )
 }
 
