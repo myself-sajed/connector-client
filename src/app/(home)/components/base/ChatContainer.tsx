3cb4b15f1182ee331e611b8f243cd1d77f65c839
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-22 23:33:36 +0530

    code fix

--- src/app/(home)/components/base/ChatContainer.tsx
+++ src/app/(home)/components/base/ChatContainer.tsx
@@ -6,15 +6,12 @@ import Empty from "./Empty"
 import SendMessageInput from "./SendMessageInput"
 import { RootState } from "@/redux/store"
 import { useSelector } from "react-redux"
-import { Message } from "@/lib/types"
 
 
 const ChatContainer = () => {
 
     const [messages, setMessages] = useState({})
     const selectedContact = useSelector((state: RootState) => state.active?.selectedContact)
-
-
 
     return (
         <div className="relative rounded-xl bg-muted/50 p-4 col-span-2 min-h-[calc(100vh-90px)] max-h-[calc(100vh-90px)] overflow-hidden">
