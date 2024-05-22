46c4ac88d3ee51212a33f491f70cc79c84e92d99
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-22 17:49:59 +0530

    add the action to the chat button

--- src/app/(home)/components/base/Empty.tsx
+++ src/app/(home)/components/base/Empty.tsx
@@ -1,8 +1,16 @@
+"use client"
+
 import { Button } from '@/components/ui/button'
+import { tabs } from '@/lib/constants'
+import { setCurrentTab } from '@/redux/slices/activeSlice'
 import { ChevronRight, Lock, Waypoints } from 'lucide-react'
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
 const Empty = () => {
+
+    const dispatch = useDispatch()
+
     return (
         <div className="w-full h-full text-center">
             <Waypoints strokeWidth={2} size={550} className="text-gray-100 absolute z-20 top-0 left-0 right-0 mx-auto" />
@@ -13,7 +21,7 @@ const Empty = () => {
                 <p>The most trusted & secure chatting platform for everyone.</p>
 
 
-                <Button className='mt-10 flex items-center justify-center gap-5'><span>Start Chatting</span><ChevronRight strokeWidth={2.5} size={17} /></Button>
+                <Button onClick={() => dispatch(setCurrentTab(tabs.CONTACTS))} className='mt-10 flex items-center justify-center gap-5'><span>Start Chatting</span><ChevronRight strokeWidth={2.5} size={17} /></Button>
                 <div className="flex items-center gap-2 justify-center text-muted-foreground text-sm mt-[7rem]">
                     <Lock size={13} /> Your chats are end-to-end encrypted.
                 </div>
