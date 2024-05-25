273bd3e122a1ae464f32cb5d82f5d0ed33d8d0cd
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-25 15:29:50 +0530

    add create chat api endpoint

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -9,5 +9,7 @@ const api = axios.create({
 // api requests
 export const getUsers = (user: string | null) => api.get(`/api/users/${user}`);
 export const getChats = (meId: string | null) => api.get(`/api/chats/${meId}`);
-export const getMessages = (contactId: string | null, meId: string | null) =>
-  api.get(`/api/messages/${contactId}/${meId}`);
+export const createChat = (contactId: string, meId: string) =>
+  api.post(`/api/chats/create`, { contactId, meId });
+export const getMessages = (chatId: string) =>
+  api.get(`/api/messages/${chatId}`);
