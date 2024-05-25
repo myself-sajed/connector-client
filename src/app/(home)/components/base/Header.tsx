0800a9388c0cc0a10998469bac6321b0f3b248bf
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-25 15:26:38 +0530

    fix padding in sm mode

--- src/app/(home)/components/base/Header.tsx
+++ src/app/(home)/components/base/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
 
 
     return (
-        <header className="sticky top-0 z-10 h-[57px] items-center gap-4 flex justify-between md:grid md:grid-cols-3 border-b bg-background px-4">
+        <header className="sticky top-0 z-10 h-[57px] items-center gap-4 flex justify-between md:grid md:grid-cols-3 border-b bg-background px-1.5 md:px-4">
             <LogoHeading className="hidden md:flex md:items-center" currentTab={currentTab} />
             <SelectedContact />
             <LogoHeading className="block md:hidden" currentTab={currentTab} />
