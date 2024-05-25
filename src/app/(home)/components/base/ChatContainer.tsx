8cc3fef7f892bdedfbf3bfff4216f90e4754b8f8
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-25 15:25:52 +0530

    add selectedchat conditions

--- src/app/(home)/components/base/ChatContainer.tsx
+++ src/app/(home)/components/base/ChatContainer.tsx
@@ -5,24 +5,31 @@ import ChatSection from "./ChatSection"
 import Empty from "./Empty"
 import SendMessageInput from "./SendMessageInput"
 import { RootState } from "@/redux/store"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { cn } from "@/lib/utils"
+import Loading from "@/components/ui/loading"
 
 
 const ChatContainer = ({ className }: { className?: string }) => {
 
     const [messages, setMessages] = useState({})
     const selectedContact = useSelector((state: RootState) => state.active?.selectedContact)
+    const selectedChat = useSelector((state: RootState) => state.active?.selectedChat)
+
 
     return (
         <div className={cn("relative rounded-xl bg-muted/50 col-span-2 p-4 min-h-[calc(100vh-89px)] max-h-[calc(100vh-89px)] overflow-hidden w-full", className)}>
             {
-                selectedContact
+                (selectedContact && selectedChat && !selectedChat.generateChatId && selectedChat.openChatSection)
                     ? <>
-                        <ChatSection messages={messages} setMessages={setMessages} selectedContact={selectedContact} />
-                        <SendMessageInput selectedContact={selectedContact} setMessages={setMessages} />
+                        <ChatSection messages={messages} setMessages={setMessages} selectedChat={selectedChat} />
+                        <SendMessageInput selectedChat={selectedChat} />
                     </>
-                    : <Empty />
+                    : selectedChat && selectedChat.generateChatId && selectedChat.openChatSection
+                        ? <div>
+                            <Loading title="Connector is creating connection, please wait..." />
+                        </div>
+                        : <Empty />
             }
         </div>
     )
