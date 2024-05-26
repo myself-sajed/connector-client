f106a27b44a00a3c8a6b2f5a719aec77865b9ec2
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-26 18:24:14 +0530

    optimize components

--- src/app/(home)/components/base/UserChatBar.tsx
+++ src/app/(home)/components/base/UserChatBar.tsx
@@ -4,19 +4,21 @@ import { getChats } from "@/lib/api"
 import { useQuery } from "@tanstack/react-query"
 import { Badge } from "@/components/ui/badge"
 import Loading from "@/components/ui/loading"
-import { Chat } from "@/lib/types"
+import { Chat, UnreadCount } from "@/lib/types"
 import ChatUserCard from "../unit/ChatUserCard"
 import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "@/redux/store"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useCallback } from "react"
 import socket from "@/lib/client-socket"
 import EmptyBar from "../unit/EmptyBar"
 import { tabs } from "@/lib/constants"
 import SearchBar from "../unit/SearchBar"
 import { setStoreChats } from "@/redux/slices/chatSlice"
+import { setSelectedChat } from "@/redux/slices/activeSlice"
+import { debounce } from 'lodash'
 
 const UserChatBar = () => {
-    const userId = useSelector((state: RootState) => state.user?.user) || null
+    const userId = useSelector((state: RootState) => state.user.user) || null
     const [chats, setChats] = useState<Chat[]>([])
     const selectedContact = useSelector((state: RootState) => state.active?.selectedContact)
     const dispatch = useDispatch()
@@ -35,62 +37,80 @@ const UserChatBar = () => {
         };
     }, [userId]);
 
-    const registerUser = () => {
+    const registerUser = useCallback(() => {
         if (userId) {
             socket.emit('register', userId);
         }
-    };
+    }, [userId]);
 
-    const handleVisibilityChange = () => {
+    const handleVisibilityChange = useCallback(() => {
         if (document.visibilityState === 'visible') {
             registerUser();
         }
-    };
-
+    }, [registerUser]);
 
     const { data: serverChats, isLoading, isError, isFetching, refetch } = useQuery({
-        queryKey: ['chat-list'],
-        queryFn: () => getChats(userId)
-    })
+        queryKey: ['chat-list', userId],
+        queryFn: () => getChats(userId),
+        refetchOnWindowFocus: false,
+        staleTime: 600000,
+        enabled: !!userId, // Only run the query if userId is available
+    });
 
     useEffect(() => {
         if (serverChats?.data) {
             setChats(serverChats?.data)
         }
-    }, [serverChats?.data])
+    }, [serverChats?.data]);
 
+    const handleServerChat = useCallback(({ chat, shouldUpdateCount }: { chat: Chat, shouldUpdateCount: boolean }) => {
+        setChats((prevChats) => {
+            let oldCount = 0
+            const filteredPrevChats = prevChats.filter((itemChat) => {
+                if (itemChat._id === chat._id) {
+                    oldCount = itemChat.unreadCount ? (itemChat.unreadCount[chat.contact._id] || 0) : 0
+                }
+                return itemChat._id !== chat._id
+            });
+            return [{ ...chat, unreadCount: { [chat.contact._id]: shouldUpdateCount ? 0 : oldCount + 1 } }, ...filteredPrevChats]
+        })
+    }, []);
 
     useEffect(() => {
-        const handleServerChat = (chat: Chat) => {
-            setChats((prevChats) => {
-                const filteredPrevChats = prevChats.filter((itemChat) => itemChat._id !== chat._id)
-                return [chat, ...filteredPrevChats]
-            })
-        };
-
         socket.on('chat:server', handleServerChat);
-
         return () => {
             socket.off('chat:server', handleServerChat);
         };
-    }, [userId, socket]);
+    }, [handleServerChat]);
 
-
-
-    const onSearch = () => {
-
-    }
+    const handleChatUnreadCount = useCallback(({ chatId, unreadCount }: { chatId: string, unreadCount: { [key: string]: number } }) => {
+        setChats((prev: Chat[]) => {
+            return prev.map((singleChat) => singleChat._id === chatId ? { ...singleChat, unreadCount: { [singleChat.contact._id]: 0 } } : singleChat);
+        });
+    }, []);
 
     useEffect(() => {
-        if (chats) {
-            dispatch(setStoreChats(chats))
-        }
-    }, [chats])
+        socket.on("chat:unreadCount", handleChatUnreadCount);
+        return () => {
+            socket.off('chat:unreadCount', handleChatUnreadCount);
+        };
+    }, [handleChatUnreadCount]);
 
+    const debouncedRefetch = useCallback(debounce(refetch, 300), []);
+
+    useEffect(() => {
+        if (chats.length) {
+            dispatch(setStoreChats(chats));
+        }
+    }, [chats, dispatch]);
+
+    const onSearch = () => {
+        debouncedRefetch();
+    }
 
     return (
         <div className="relative flex-col items-start gap-2 flex h-full">
-            <SearchBar tooltipText="Refresh Chats" isLoading={isLoading} isFetching={isFetching} refetch={refetch} onSearch={onSearch} />
+            <SearchBar tooltipText="Refresh Chats" isLoading={isLoading} isFetching={isFetching} refetch={debouncedRefetch} onSearch={onSearch} />
             {
                 isError
                     ? <Badge variant="destructive" className="my-10">
@@ -98,23 +118,22 @@ const UserChatBar = () => {
                     </Badge>
                     : isLoading
                         ? <Loading title="Fetching Chats..." />
-                        : chats.filter((chat) => chat.chatable)?.length > 0 ? <div className="divide-y overflow-y-auto w-full overflow-hidden min-h-[calc(100vh-137px)] max-h-[calc(100vh-137px)]">
-                            {
-                                chats.filter((chat) => chat.chatable)?.map((chat: Chat) => {
-                                    return <ChatUserCard key={chat._id} chat={chat} isSelected={chat.contact._id === selectedContact?._id} />
-                                })
-                            }
-                        </div>
-
-                            :
-
+                        : chats.filter((chat) => chat.chatable)?.length > 0 ? (
+                            <div className="divide-y overflow-y-auto w-full overflow-hidden min-h-[calc(100vh-137px)] max-h-[calc(100vh-137px)]">
+                                {
+                                    chats.filter((chat) => chat.chatable)?.map((chat: Chat) => (
+                                        <ChatUserCard key={chat._id} chat={chat} isSelected={chat.contact._id === selectedContact?._id} />
+                                    ))
+                                }
+                            </div>
+                        ) : (
                             <div className="overflow-y-auto w-full overflow-hidden min-h-[calc(100vh-137px)] max-h-[calc(100vh-137px)] pr-5">
                                 <EmptyBar type={tabs.CHATS} />
                             </div>
+                        )
             }
-
         </div>
     )
 }
 
-export default UserChatBar
+export default UserChatBar;
