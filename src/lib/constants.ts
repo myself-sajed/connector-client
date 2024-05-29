8087c00695e1158fbfbc1d5e81dac96573603598
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-29 15:28:38 +0530

    add activities constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,6 +2,12 @@ export const tabs = {
   CHATS: "Chats",
   CONTACTS: "Contacts",
   PROFILE: "Profile",
+  LOGOUT: "Logout",
+} as const;
+
+export const activities = {
+  CHAT: "Chat",
+  CODE: "Code",
 } as const;
 
 export type TabType = (typeof tabs)[keyof typeof tabs];
