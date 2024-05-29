112580b612bebf3b6dbb0781bc0abcce574cd766
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-29 15:28:05 +0530

    add logout functionality in sidebar

--- src/app/(home)/components/base/Sidebar.tsx
+++ src/app/(home)/components/base/Sidebar.tsx
@@ -1,5 +1,6 @@
 import {
     CircleDashed,
+    LogOut,
     MessageCircle,
     Settings,
     SquareUser,
@@ -18,6 +19,12 @@ import { tabs } from "@/lib/constants"
 import { useDispatch, useSelector } from "react-redux"
 import { setContact, setCurrentTab, setSelectedChat } from "@/redux/slices/activeSlice"
 import { RootState } from "@/redux/store"
+import { useState } from "react"
+import { logout } from "@/lib/api"
+import { useRouter } from "next/navigation"
+import { toast } from "sonner"
+
+
 
 const upperTabs = [
     {
@@ -35,6 +42,10 @@ const lowerTabs = [
         name: tabs.PROFILE,
         icon: <SquareUser className="size-5" />,
     },
+    {
+        name: tabs.LOGOUT,
+        icon: <LogOut className="size-5" />,
+    },
 ]
 
 
@@ -42,9 +53,35 @@ const Sidebar = () => {
 
     const dispatch = useDispatch()
     const currentTab = useSelector((state: RootState) => state.active?.currentTab || tabs.CHATS)
+    const [isLoading, setIsLoading] = useState(false)
+    const router = useRouter()
+
 
     const handleTabChange = (tabName: string) => {
         dispatch(setCurrentTab(tabName))
+    }
+
+    const handleLogout = async () => {
+        try {
+            setIsLoading(true)
+            const res = await logout()
+            if (res.data.status === "success") {
+                router.push('/login')
+
+                dispatch(setSelectedChat(null))
+                dispatch(setContact(null))
+                dispatch(setCurrentTab(tabs.CHATS))
+
+                toast.success("Logout successfully")
+            } else {
+                toast.error("Could not logout, please try again...")
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error("Could not logout, please try again...")
+        } finally {
+            setIsLoading(false)
+        }
     }
 
     return (
@@ -92,7 +129,7 @@ const Sidebar = () => {
                                         className={`rounded-lg`}
                                         aria-label={tab.name}
                                         onClick={() => {
-                                            handleTabChange(tab.name);
+                                            tab.name === tabs.LOGOUT ? handleLogout() : handleTabChange(tab.name);
                                             dispatch(setSelectedChat(null))
                                             dispatch(setContact(null))
                                         }}
