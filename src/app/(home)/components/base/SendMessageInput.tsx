1ebea6d557a0ccaf9e9dcfbae05518f53a331da7
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-29 17:31:07 +0530

    add typing and online offline status functionality

--- src/app/(home)/components/base/SendMessageInput.tsx
+++ src/app/(home)/components/base/SendMessageInput.tsx
@@ -1,23 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client"
 import {
-    Mic,
-    Paperclip,
     Send,
 } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 import socket from "@/lib/client-socket"
-import { Dispatch, SetStateAction, useEffect, useRef } from "react"
+import { Dispatch, SetStateAction, useCallback, useEffect, useRef } from "react"
 import { RootState } from "@/redux/store"
 import { useSelector } from "react-redux"
 import { SelectedChat } from "@/redux/slices/activeSlice"
 import { v4 as uuid } from "uuid"
-import { Message } from "@/lib/types"
 import { MessagesState } from "./ChatContainer"
-import VoiceRecorder from "./VoiceRecorder"
+import { debounce, throttle } from "lodash"
 
 interface PropType {
     selectedChat: SelectedChat;
@@ -25,21 +21,19 @@ interface PropType {
 }
 
 const SendMessageInput = ({ selectedChat, setMessages }: PropType) => {
-
-    const messageRef = useRef<HTMLTextAreaElement>(null)
-    const userId = useSelector((state: RootState) => state.user.user)?._id
+    const messageRef = useRef<HTMLTextAreaElement>(null);
+    const userId = useSelector((state: RootState) => state.user.user)?._id;
 
     useEffect(() => {
-        messageRef.current?.focus()
-    }, [userId])
+        messageRef.current?.focus();
+    }, [userId]);
 
     const handleSubmit = () => {
-        const msgValue = messageRef.current?.value
+        const msgValue = messageRef.current?.value;
 
         if (msgValue) {
-
             // optimistic message
-            const tempMessageId = uuid()
+            const tempMessageId = uuid();
 
             const optimisticMessage = {
                 _id: tempMessageId,
@@ -48,78 +42,58 @@ const SendMessageInput = ({ selectedChat, setMessages }: PropType) => {
                 author: { _id: userId },
                 status: "optimistic",
                 showChevron: false,
-                isEdited: false
-            }
-
+                isEdited: false,
+            };
 
             const chatData = {
                 selectedChat,
                 messageContent: msgValue,
                 author: userId,
-                tempMessageId
-            }
+                tempMessageId,
+            };
 
-            socket.emit('message:client', chatData)
+            socket.emit("message:client", chatData);
             if (selectedChat && selectedChat._id) {
                 const chatId = selectedChat._id as string;
                 setMessages((prev: MessagesState) => {
                     return {
                         ...prev,
-                        [chatId]: [...(prev[chatId] || []), optimisticMessage]
+                        [chatId]: [...(prev[chatId] || []), optimisticMessage],
                     };
                 });
             } else {
                 console.error("Selected chat or chat ID is null or undefined");
             }
-            messageRef.current.value = ''
+            messageRef.current.value = "";
         }
-    }
+    };
+
+    const handleTyping = useCallback(
+        throttle(() => {
+            socket.emit("client:typing", { _id: selectedChat._id, contact: { _id: selectedChat?.contact?._id } });
+        }, 1000), // Throttle typing event to emit at most once every second
+        [selectedChat]
+    );
+
+    const handleStoppedTyping = debounce(() => {
+        socket.emit("client:stoppedTyping", selectedChat);
+    }, 1000); // Debounce stopped typing event to emit after 1 second of inactivity
 
     const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-        if (event.key === 'Enter' && !event.shiftKey) {
+        if (event.key === "Enter" && !event.shiftKey) {
             event.preventDefault(); // Prevent newline
             handleSubmit();
-        }
-    };
-
-    const handleVoiceStop = (audioBlob: Blob) => {
-        const tempMessageId = uuid();
-        const optimisticMessage = {
-            _id: tempMessageId,
-            audioUrl: URL.createObjectURL(audioBlob),
-            updatedAt: new Date().getTime(),
-            author: { _id: userId },
-            status: "optimistic",
-            showChevron: false,
-            isEdited: false
-        };
-
-        const chatData = {
-            selectedChat,
-            audioContent: audioBlob,
-            author: userId,
-            tempMessageId
-        };
-
-        socket.emit('voiceMessage:client', chatData);
-        if (selectedChat && selectedChat._id) {
-            const chatId = selectedChat._id as string;
-            setMessages((prev: MessagesState) => {
-                return {
-                    ...prev,
-                    [chatId]: [...(prev[chatId] || []), optimisticMessage]
-                };
-            });
         } else {
-            console.error("Selected chat or chat ID is null or undefined");
+            handleTyping();
+            handleStoppedTyping();
         }
     };
-
-
     return (
         <div className="relative rounded-lg border bg-background mt-2 flex items-start">
-            <Textarea ref={messageRef}
+            <Textarea
+                ref={messageRef}
                 onKeyDown={handleKeyDown}
+                onKeyUp={handleKeyDown}
                 autoFocus={true}
                 placeholder="Type your message here..."
                 className="min-h-32 resize-none border-none flex-1 p-3 outline-none focus-visible:ring-0 focus-visible:outline-none focus-visible:ring-offset-0"
@@ -129,7 +103,7 @@ const SendMessageInput = ({ selectedChat, setMessages }: PropType) => {
                 <Send className="size-3.5" />
             </Button>
         </div>
-    )
-}
+    );
+};
 
-export default SendMessageInput
+export default SendMessageInput;
--- src/app/(home)/components/unit/SelectedContact.tsx
+++ src/app/(home)/components/unit/SelectedContact.tsx
@@ -1,16 +1,22 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client"
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import socket from "@/lib/client-socket"
 import { Contact } from "@/lib/types"
 import { cn } from "@/lib/utils"
-import { setContact, setSelectedChat } from "@/redux/slices/activeSlice"
+import { SelectedChat, setContact, setSelectedChat } from "@/redux/slices/activeSlice"
 import { RootState } from "@/redux/store"
 import { ArrowLeft } from "lucide-react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 
 const SelectedContact = ({ className }: { className?: string }) => {
+    const [onlineStatus, setOnlineStatus] = useState<string | null>(null)
+    const [typingStatus, setTypingStatus] = useState<string | null>(null)
+    const selectedChat = useSelector((state: RootState) => state.active?.selectedChat)
+    const selectedContact = selectedChat?.contact
 
-    const selectedContact = useSelector((state: RootState) => state.active?.selectedContact)
     const dispatch = useDispatch()
     const handleBack = () => {
 
@@ -20,6 +26,48 @@ const SelectedContact = ({ className }: { className?: string }) => {
         }
 
     }
+
+    useEffect(() => {
+
+        const handleStatus = ({ status, statusOf }: { status: string, statusOf: string }) => {
+            console.log(status, statusOf)
+            if (statusOf === selectedContact?._id) {
+                setOnlineStatus(status)
+            }
+        }
+
+        socket.on("server:online-status", handleStatus)
+
+        return () => {
+            socket.on("server:online-status", handleStatus)
+        }
+    }, [])
+
+
+    useEffect(() => {
+
+        const handleTyping = ({ selectedChatFromServer, typingStatus }: { selectedChatFromServer: SelectedChat, typingStatus: string }) => {
+            if (selectedChat) {
+                if (selectedChatFromServer._id === selectedChat._id) {
+                    setTypingStatus(typingStatus)
+                }
+            }
+        }
+
+        socket.on("server:typing", handleTyping)
+
+        return () => {
+            socket.on("server:typing", handleTyping)
+        }
+    }, [])
+
+    useEffect(() => {
+        if (selectedContact) {
+            socket.emit("client:online-status", selectedContact._id)
+        }
+    }, [selectedContact])
+
+
 
     return (selectedContact && <div className={cn("flex items-center gap-3", className)}>
         <div className="flex items-center gap-2 hover:bg-muted md:hover:bg-transparent rounded-full cursor-pointer p-2" onClick={handleBack}>
@@ -31,7 +79,7 @@ const SelectedContact = ({ className }: { className?: string }) => {
         </div>
         <div className="flex flex-col">
             <p className="font-semibold" >{selectedContact.name}</p>
-            <span className="text-xs text-muted-foreground" style={{ lineHeight: '14px' }}>online</span>
+            <span className="text-xs text-muted-foreground" style={{ lineHeight: '14px' }}>{typingStatus || onlineStatus}</span>
         </div>
     </div>
     )
