f4793b54f7ee71a80aa3878507d2beba5b71b87d
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-29 20:13:50 +0530

    add hero for login registration

---
+++ src/app/(home)/login/components/LoginRegisterHero.tsx
@@ -0,0 +1,19 @@
+import { Lock, Waypoints } from 'lucide-react'
+import React from 'react'
+
+const LoginRegisterHero = () => {
+    return (
+        <div className="relative z-30 flex items-center justify-center flex-col h-full">
+            <Waypoints strokeWidth={2} size={100} className="text-primary" />
+            <p style={{ lineHeight: '85px' }} className="text-[5rem] font-bold mt-10 text-primary">Connector</p>
+            <p>The most trusted & secure chatting platform for everyone.</p>
+
+
+            <div className="flex items-center gap-2 justify-center text-muted-foreground text-sm mt-[7rem]">
+                <Lock size={13} /> Your chats are end-to-end encrypted.
+            </div>
+        </div>
+    )
+}
+
+export default LoginRegisterHero
