2a5fbda527b9dee59ade93b00abf79a85aff97e7
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-29 15:27:49 +0530

    create code and chat tab in header

--- src/app/(home)/components/base/Header.tsx
+++ src/app/(home)/components/base/Header.tsx
@@ -1,14 +1,16 @@
-import { tabs } from "@/lib/constants"
+import { activities, tabs } from "@/lib/constants"
 import { RootState } from "@/redux/store"
 import { useDispatch, useSelector } from "react-redux"
 import SelectedContact from "../unit/SelectedContact"
 import LogoHeading from "../unit/LogoHeading"
 import { Button } from "@/components/ui/button"
-import { MessageCircleX } from "lucide-react"
-import { setContact, setSelectedChat } from "@/redux/slices/activeSlice"
+import { LogOut, MessageCircleX, MessageSquareCode, MessageSquareText } from "lucide-react"
+import { setContact, setCurrentActivity, setSelectedChat } from "@/redux/slices/activeSlice"
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tooltip, TooltipContent, TooltipProvider } from "@/components/ui/tooltip"
+import { TooltipTrigger } from "@radix-ui/react-tooltip"
 
 const Header = () => {
-
     const currentTab = useSelector((state: RootState) => state.active?.currentTab || tabs.CHATS)
     const selectedChat = useSelector((state: RootState) => state.active.selectedChat)
     const dispatch = useDispatch()
@@ -17,14 +19,52 @@ const Header = () => {
         dispatch(setContact(null))
     }
 
+
+
+
+
     return (
         <header className="sticky top-0 z-10 h-[57px] items-center gap-4 flex justify-between md:grid md:grid-cols-3 border-b bg-background px-1.5 md:px-4">
             <LogoHeading className="hidden md:flex md:items-center" currentTab={currentTab} />
-            <div className="w-full flex items-center justify-between col-span-2">
-                <SelectedContact />
-                {selectedChat && <Button onClick={closeChat} variant="secondary" className="hidden items-center gap-2 md:flex"><MessageCircleX size={17} /><span>Close Chat</span></Button>}
-            </div>
-            <LogoHeading className="block md:hidden" currentTab={currentTab} />
+            {selectedChat &&
+                <div className="w-full flex items-center justify-between col-span-2">
+                    <SelectedContact />
+                    <div className="flex items-center gap-2 justify-end">
+                        <Tabs onValueChange={(tab) => dispatch(setCurrentActivity(tab))} defaultValue={activities.CHAT}>
+                            <TabsList>
+                                <TabsTrigger value={activities.CHAT}>
+                                    <TooltipProvider>
+                                        <Tooltip>
+                                            <TooltipTrigger>
+                                                <MessageSquareText size={20} />
+                                            </TooltipTrigger>
+                                            <TooltipContent>
+                                                Chat
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>
+                                </TabsTrigger>
+                                <TabsTrigger value={activities.CODE}>
+                                    <TooltipProvider>
+                                        <Tooltip>
+                                            <TooltipTrigger>
+                                                <MessageSquareCode size={20} />
+                                            </TooltipTrigger>
+                                            <TooltipContent>
+                                                Code
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>
+                                </TabsTrigger>
+                            </TabsList>
+                        </Tabs>
+                        <Button onClick={closeChat} variant="secondary" className="hidden items-center gap-2 md:flex"><MessageCircleX size={17} /><span>Close Chat</span></Button>
+                    </div>
+                </div>
+            }
+
+            <LogoHeading className="hidden sm:block md:hidden" currentTab={currentTab} />
+
         </header>
     )
 }
