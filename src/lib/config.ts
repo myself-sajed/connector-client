48cfe103d3152c6928d5110836b7dc3d093c5681
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-06-06 08:59:34 +0530

    update the config files and change the client url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,11 @@
+// const config = {
+//   BACKEND_URL: "http://localhost:4000",
+//   FRONTEND_URL: "http://localhost:3000",
+// };
+
 const config = {
   BACKEND_URL: "https://connector-server-00zk.onrender.com",
-  FRONTEND_URL: "https://connector-sajed.vercel.app",
+  FRONTEND_URL: "https://connector-b78z.onrender.com",
 };
 
 export default config;
