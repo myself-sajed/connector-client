fa4c13e2fe3d82adc89a2fff39d9ad566b6045f4
Shaikh Sajed <85938068+myself-sajed@users.noreply.github.com>
2024-06-07 09:29:29 +0530

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -5,7 +5,7 @@
 
 const config = {
   BACKEND_URL: "https://connector-server-00zk.onrender.com",
-  FRONTEND_URL: "https://connector-b78z.onrender.com",
+  FRONTEND_URL: "https://connector-sajed.vercel.app",
 };
 
 export default config;
