040d63f5ebf5b1e53c210b91ba35750cd6aa337a
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-06-07 09:52:41 +0530

    change the connector tagline in login and signup

--- src/app/(home)/login/components/LoginRegisterHero.tsx
+++ src/app/(home)/login/components/LoginRegisterHero.tsx
@@ -6,7 +6,7 @@ const LoginRegisterHero = () => {
         <div className="relative z-30 flex items-center justify-center flex-col h-full">
             <Waypoints strokeWidth={2} size={100} className="text-primary" />
             <p style={{ lineHeight: '85px' }} className="text-[5rem] font-bold mt-10 text-primary">Connector</p>
-            <p>The most trusted & secure chatting platform for everyone.</p>
+            <p>The most efficient chatting & code sharing platform for everyone.</p>
 
 
             <div className="flex items-center gap-2 justify-center text-muted-foreground text-sm mt-[7rem]">
